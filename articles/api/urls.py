37fc3846275af2562a1ee227919074e8082bf6d0
parthsujalshah <parthsujalshah@gmail.com>
2020-06-07 14:26:18 +0530

    create, update, delete views

--- articles/api/urls.py
+++ articles/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
   path('', ArticleListView.as_view()),
+  path('create/', ArticleCreateView.as_view()),
   path('<pk>', ArticleDetailView.as_view()),
+  path('<pk>/update/', ArticleUpdateView.as_view()),
+  path('<pk>/delete/', ArticleDeleteView.as_view()),
 ]
--- articles/api/views.py
+++ articles/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
 from ..models import Article
 from .serializers import ArticleSerializer
 
@@ -7,5 +7,17 @@ class ArticleListView(ListAPIView):
   serializer_class = ArticleSerializer
 
 class ArticleDetailView(RetrieveAPIView):
+  queryset = Article.objects.all()
+  serializer_class = ArticleSerializer
+
+class ArticleCreateView(CreateAPIView):
+  queryset = Article.objects.all()
+  serializer_class = ArticleSerializer
+
+class ArticleUpdateView(UpdateAPIView):
+  queryset = Article.objects.all()
+  serializer_class = ArticleSerializer
+
+class ArticleDeleteView(DestroyAPIView):
   queryset = Article.objects.all()
   serializer_class = ArticleSerializer
