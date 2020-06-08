a6fcc56009ae6a88b96b1d589cad73d5dcd30bbe
parthsujalshah <parthsujalshah@gmail.com>
2020-06-08 23:11:00 +0530

    provider to app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import reducer from './store/reducers/auth';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)));
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  app,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
